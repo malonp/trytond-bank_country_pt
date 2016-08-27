895fa744ab48c3e8406e9526c0358a57a3c78c66
M. Alonso <port02.server@gmail.com>
2016-08-27 12:20:39 +0100

    first commit

---
+++ banknumber_pt.py
@@ -0,0 +1,36 @@
+##############################################################################
+#
+#    GNU Condo: The Free Management Condominium System
+#    Copyright (C) 2016- M. Alonso <port02.server@gmail.com>
+#
+#
+#    This program is free software: you can redistribute it and/or modify
+#    it under the terms of the GNU General Public License as published by
+#    the Free Software Foundation, either version 3 of the License, or
+#    (at your option) any later version.
+#
+#    This program is distributed in the hope that it will be useful,
+#    but WITHOUT ANY WARRANTY; without even the implied warranty of
+#    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#    GNU General Public License for more details.
+#
+#    You should have received a copy of the GNU General Public License
+#    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+##############################################################################
+
+
+from stdnum.iso7064 import mod_97_10
+
+from trytond.modules.bank_country import bank, banknumber
+
+
+def check_code(number):
+    '''
+    Check Portuguesse Bank code.
+    '''
+    return mod_97_10.is_valid(number)
+
+
+banknumber.check_code_pt = check_code
+bank.BANK_COUNTRIES.append('PT')
